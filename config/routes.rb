98b4adc48ba7a75561dac32c0fe3c346024be355
mayaSun <maya166@gmail.com>
2016-02-03 20:46:52 +0200

    Fix admin check permition url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'pages#home'
+  get '/', to: 'pages#home'
+
+
   devise_for :admins
   devise_for :brokers
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root to: 'pages#home'
 end
