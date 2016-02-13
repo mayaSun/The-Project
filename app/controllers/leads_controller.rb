802771392e8a3d0dc046b27f078e6c609c336542
mayaSun <maya166@gmail.com>
2016-02-13 23:13:35 +0200

    add xml response to lead create

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -5,14 +5,16 @@ class LeadsController <ApplicationController
 
   def create
     xml = Builder::XmlMarkup.new
-    xml.instruct!(:xml, :encoding => "UTF-8")   
+    xml.instruct!(:xml, :version=>"1.0", :encoding => "UTF-8")
     if Lead.create(name: params[:name], email: params[:email], phone: params[:phone], country_id: 1)
       site = Site.first
     else
       site = Site.first
     end
-    xml.site(:site => site.url, :name => site.name)
-    render xml: xml
+    xml.site { |s| s.url(site.url); s.name(site.name) }
+    render xml: xml    
   end
 
 end
+
+
