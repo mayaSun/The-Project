722d6b6fb98a2b61d1c592c05d5bd1c70af5eee3
mayaSun <maya166@gmail.com>
2016-02-23 21:15:07 +0200

    Add country code

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -31,9 +31,13 @@ class LeadsController <ApplicationController
     end
     xml = Builder::XmlMarkup.new(:indent => 2)
     xml.instruct!(:xml, :version=>"1.0", :encoding => "UTF-8", :standalone => "yes")
-    xml.lead { |s| s.username(lead.email) }
+    #xml.lead { |s| s.username(lead.email), s.password(lead.password) }
+    
+    xml.site(:site => site.url, :name => site.name)
+    
     render xml: xml.target!  
   end
 
 end
+
 
